2b6012eb9931d0ceb2db17dad0e19123e0027c6a
akanevrc <akanezoranomukou@gmail.com>
2023-05-03 11:27:59 +0900

    test: テスト環境を追加

---
+++ Assets/akanevrc/TextureProxy/Editor/Tests/TextureProxyImporterTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace akanevrc.TextureProxy.Tests
+{
+    public class TextureProxyImporterTest
+    {
+        [Test]
+        public void AlwaysBeSuccessful()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
